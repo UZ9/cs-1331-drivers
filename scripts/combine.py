f68cf4319aaf7bc0184b13dd8af01e525ff6765c
Justin Hwang <justin@goliath.org>
2023-10-20 03:01:42 -0400

    Reorganized file structure for more intuitive usage

--- scripts/combine.py
+++ scripts/combine.py
@@ -1,11 +1,11 @@
 import glob
 import os
 
-filename = "./output/TestDriver.java"
+filename = "../Driver.java"
 
 os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-read_files = glob.glob("*.java")
+read_files = glob.glob("../src/*.java")
 
 with open(filename, "wb") as outfile:
     for f in read_files:
