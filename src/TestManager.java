4c72dfe2b739518e2c356b982e4782bab002304b
Yerti <36551149+UZ9@users.noreply.github.com>
2023-10-20 16:40:04 -0400

    :hammer: Move reflection code to generic system
    Other: Added StringUtils with ability to print horizontal line

--- src/TestManager.java
+++ src/TestManager.java
@@ -1,42 +1,59 @@
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 class TestManager {
     public static void main(String[] args) {
 
-        StudentTests studentTests = new StudentTests();
-        
-        for (Method m : studentTests.getClass().getMethods()) {
-            TestCase testCase = (TestCase) m.getAnnotation(TestCase.class);
-            Tip tip = (Tip) m.getAnnotation(Tip.class);
+        List<Class<?>> testClasses = new ArrayList<>();
 
+        // TODO: Move to annotation scanning
+        testClasses.add(StudentTests.class);
 
-            if (testCase != null) {
-                try {
-                    System.out.println("Testing: " + testCase.name());
+        for (Class<?> testClass : testClasses) {
+            try {
 
-                    m.invoke(studentTests);
+                Object instance = testClass.getDeclaredConstructor().newInstance();
 
-                    System.out.println("Success!");
-                } catch (InvocationTargetException e) {
-                    if (e.getCause() instanceof TestFailedException) {
-                        TestFailedException tfe = (TestFailedException) e.getCause();
+                for (Method m : testClass.getMethods()) {
+                    TestCase testCase = (TestCase) m.getAnnotation(TestCase.class);
+                    Tip tip = (Tip) m.getAnnotation(Tip.class);
 
-                        System.out.println(ColorUtils.formatColorString(AsciiColorCode.BRIGHT_RED_BACKGROUND, AsciiColorCode.BRIGHT_WHITE_FOREGROUND, " FAILED: \u00BB ") + " " + tfe.getMessage());
-                        if (tip != null) System.out.println("HINT: " + tip.tip());
-                    } else {
-                        e.getCause().printStackTrace();
+                    if (testCase != null) {
+                        try {
+                            StringUtils.printHorizontalLine();
+
+                            System.out.println("Testing: " + testCase.name());
+
+                            m.invoke(instance);
+
+                            System.out.println("Success!");
+                        } catch (InvocationTargetException e) {
+                            if (e.getCause() instanceof TestFailedException) {
+                                TestFailedException tfe = (TestFailedException) e.getCause();
+
+                                System.out.println(ColorUtils.formatColorString(AsciiColorCode.BRIGHT_RED_BACKGROUND,
+                                        AsciiColorCode.BRIGHT_WHITE_FOREGROUND, " FAILED: \u00BB ") + " "
+                                        + tfe.getMessage());
+                                if (tip != null)
+                                    System.out.println("HINT: " + tip.tip());
+                            } else {
+                                e.getCause().printStackTrace();
+                            }
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        } catch (IllegalArgumentException e) {
+                            e.printStackTrace();
+                        }
                     }
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (IllegalArgumentException e) {
-                    e.printStackTrace();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+
     }
-    
 
 }
-
-
