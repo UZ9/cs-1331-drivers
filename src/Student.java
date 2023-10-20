ea0fd3b5395ff5194ed799543168fafbcb3dd1df
Yerti <36551149+UZ9@users.noreply.github.com>
2023-10-20 17:05:56 -0400

    :art: Reformat test outputs to look much nicer

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,20 @@
 public class Student {
     private String name;
+    private int happiness;
     
     public Student(String name) {
-        name = name;
+        this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void giveCandy(int candy) {
+        happiness += candy;
+    }
+
+    public int getHappiness() {
+        return happiness;
     }
 }
