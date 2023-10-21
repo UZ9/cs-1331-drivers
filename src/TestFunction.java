7dbb76a09dd649b3029dd15b6bca3b99e6f7f140
Justin Hwang <justin@goliath.org>
2023-10-21 02:31:06 -0400

    Renamed from 'received' to 'actual'

--- src/TestFunction.java
+++ src/TestFunction.java
@@ -1,67 +1,67 @@
 class TestFunction {
     /**
      * Detects if the given Strings do not have the same content (case-sensitive)
-     * @param received
+     * @param actual
      * @param expected
      * @throws TestFailedException
      */
-    public static void assertEqual(String received, String expected) throws TestFailedException {
+    public static void assertEqual(String actual, String expected) throws TestFailedException {
         boolean failed = false;
 
-        if (received == null) {
-            failed = received != expected;
+        if (actual == null) {
+            failed = actual != expected;
         } else {
 
-            if (!received.equals(expected)) {
+            if (!actual.equals(expected)) {
                 failed = true;
             }
         }
 
         if (failed) {
-            throw new TestFailedException("Strings different! Received \"" + received + "\", expected \"" + expected + "\"");
+            throw new TestFailedException("Strings different! Received \"" + actual + "\", expected \"" + expected + "\"");
         }
     }
 
     /**
      * Detects if the given integers are not equal.
-     * @param received
+     * @param actual
      * @param expected
      * @throws TestFailedException
      */
-    public static void assertEqual(int received, int expected) throws TestFailedException {
-        boolean failed = (received != expected);
+    public static void assertEqual(int actual, int expected) throws TestFailedException {
+        boolean failed = (actual != expected);
         if (failed) {
-            throw new TestFailedException("Integer value difference: Received " + received + ", expected " + expected);
+            throw new TestFailedException("Integer value difference: Received " + actual + ", expected " + expected);
         }
     }
 
     /**
      * Detects if the given doubles are not within 1.0e-6 of one another.
-     * @param received
+     * @param actual
      * @param expected
      * @throws TestFailedException
      */
-    public static void assertEqual(double received, double expected) throws TestFailedException {
+    public static void assertEqual(double actual, double expected) throws TestFailedException {
         final double ALLOWABLE_ERROR = 0.000001;
 
-        boolean failed = (Math.abs(received - expected) > ALLOWABLE_ERROR);
+        boolean failed = (Math.abs(actual - expected) > ALLOWABLE_ERROR);
 
         if (failed) {
-            throw new TestFailedException("Double value difference: Received " + received + ", expected " + expected);
+            throw new TestFailedException("Double value difference: Received " + actual + ", expected " + expected);
         }
     }
 
     /**
      * Detects if the given booleans do not have equal values.
-     * @param received
+     * @param actual
      * @param expected
      * @throws TestFailedException
      */
-    public static void assertEqual(boolean received, boolean expected) throws TestFailedException {
-        boolean failed = (received != expected);
+    public static void assertEqual(boolean actual, boolean expected) throws TestFailedException {
+        boolean failed = (actual != expected);
 
         if (failed) {
-            throw new TestFailedException("Boolean value difference: Received " + received + ", expected " + expected);
+            throw new TestFailedException("Boolean value difference: Received " + actual + ", expected " + expected);
         }
     }
 
@@ -69,15 +69,15 @@ class TestFunction {
      * Detects if the given Exceptions are the same TYPE, not if they are caused by the same thing. Any two
      * NullPointerExceptions will, when passed into this function, return true, even if they are cuased by
      * two unrelated issues.
-     * @param received
+     * @param actual
      * @param expected
      * @throws TestFailedException
      */
-    public static void assertEqual(Exception received, Exception expected) throws TestFailedException {
-        boolean failed = (received.getCause().getClass() != expected.getCause().getClass());
+    public static void assertEqual(Exception actual, Exception expected) throws TestFailedException {
+        boolean failed = (actual.getCause().getClass() != expected.getCause().getClass());
         
         if (failed) {
-            throw new TestFailedException("Exception class difference: Received " + received.getCause().getClass() + ", but expected " + expected.getCause().getClass());
+            throw new TestFailedException("Exception class difference: Received " + actual.getCause().getClass() + ", but expected " + expected.getCause().getClass());
         }
     }
 }
