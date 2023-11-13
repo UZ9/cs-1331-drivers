6cc7aaba3e4d53526995f3cf59f1f1d0ecde74e6
Yerti <36551149+UZ9@users.noreply.github.com>
2023-11-13 18:52:50 -0500

    :sparkles: Add StackOverflowError prevention

--- src/TestContainer.java
+++ src/TestContainer.java
@@ -88,6 +88,13 @@ class TestContainer implements Runnable {
                 if (tip != null)
                     System.out.printf("\t%s\n", ColorUtils.formatColorString(AsciiColorCode.BRIGHT_WHITE_BACKGROUND,
                             AsciiColorCode.BLACK_FOREGROUND, "HINT: " + tip.description()));
+            } else if (e.getCause() instanceof StackOverflowError) {
+                e.printStackTrace();
+                
+                System.out.println(ColorUtils.formatColorString(AsciiColorCode.WHITE_BACKGROUND,
+                        AsciiColorCode.RED_FOREGROUND,
+                        "\tYour code resulted in a StackOverflowException. This is almost certainly because of a recursive method stuck in an infinite loop."));
+
             } else {
                 System.out.println(ColorUtils.formatColorString(AsciiColorCode.WHITE_BACKGROUND,
                         AsciiColorCode.RED_FOREGROUND,
